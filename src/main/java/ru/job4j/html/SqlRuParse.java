534938d47f6634583d16bbf20c49c0ee3b6aae01
RvDmitry <rvdmitry@gmail.com>
2020-09-07 01:31:11 +0600

    2.1. Преобразование даты [#305546]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -5,6 +5,11 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.sql.Timestamp;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Class SqlRuParse
  * Класс осуществляет парсинг сайта вакансий.
@@ -27,7 +32,41 @@ public class SqlRuParse {
             Element time = td.parent().child(5);
             System.out.println(href.attr("href"));
             System.out.println(href.text());
-            System.out.println(time.text());
+            System.out.println(parse(time.text()));
         }
+    }
+
+    /**
+     * Метод осуществляет преобразование даты в формате заданном на сайте в формат Timestamp,
+     * с целью дальнейшей возможности записи даты в базу данных.
+     * @param time Дата и время в формате сайта
+     * @return Дата и время в формате Timestamp
+     * @throws ParseException Исключение, при неудачном преобразовании даты и времени
+     */
+    private static Timestamp parse(String time) throws ParseException {
+        String today = "сегодня";
+        String yesterday = "вчера";
+        String fullTime = "";
+        SimpleDateFormat formatter = new SimpleDateFormat("dd MMMM yy, HH:mm");
+        String[] shortMonths = {"янв", "фев", "мар", "апр", "май", "июн", "июл", "авг", "сен",
+                "окт", "ноя", "дек"};
+        String[] months = {"января", "февраля", "марта", "апреля", "мая", "июня", "июля",
+                "августа", "сентября", "октября", "ноября", "декабря"};
+        for (int i = 0; i < shortMonths.length; i++) {
+            if (time.contains(shortMonths[i])) {
+                fullTime = time.replace(shortMonths[i], months[i]);
+                break;
+            }
+        }
+        if (time.contains(today)) {
+            fullTime = time.replace(today, new SimpleDateFormat("dd MMMM yy").format(new Date()));
+        }
+        if (time.contains(yesterday)) {
+            fullTime = time.replace(yesterday,
+                    new SimpleDateFormat("dd MMMM yy")
+                            .format(new Date(System.currentTimeMillis() - 24 * 60 * 60 * 1000)));
+        }
+        Date date = formatter.parse(fullTime);
+        return new Timestamp(date.getTime());
     }
 }
