f9011b746889a140a69d94195c6e19fb0f9d6bdc
RvDmitry <rvdmitry@gmail.com>
2020-09-13 18:01:09 +0600

    5. PsqlStore [#305541]

---
+++ src/main/java/ru/job4j/grabber/store/PsqlStore.java
@@ -0,0 +1,140 @@
+package ru.job4j.grabber.store;
+
+import ru.job4j.grabber.model.Post;
+import ru.job4j.grabber.parser.SqlRuParse;
+
+import java.io.InputStream;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+/**
+ * Class PsqlStore
+ * Класс осуществляет работу с базой данных.
+ * @author Dmitry Razumov
+ * @version 1
+ */
+public class PsqlStore implements Store, AutoCloseable {
+    /**
+     * Поле содержит параметры соединения в базой.
+     */
+    private final Connection cnn;
+
+    /**
+     * Конструктор инициализирует соединение с базой
+     * @param cfg Параметры соединения
+     */
+    public PsqlStore(Properties cfg) {
+        try {
+            Class.forName(cfg.getProperty("db.driver"));
+            cnn = DriverManager.getConnection(
+                    cfg.getProperty("db.url"),
+                    cfg.getProperty("db.username"),
+                    cfg.getProperty("db.password")
+            );
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    /**
+     * Метод сохраняет объявление в базе.
+     * @param post Объявление
+     */
+    @Override
+    public void save(Post post) {
+        try (PreparedStatement st = cnn.prepareStatement(
+                "insert into post(name, text, link, created) values(?, ?, ?, ?)")) {
+            st.setString(1, post.getTitle());
+            st.setString(2, post.getDescription());
+            st.setString(3, post.getLink());
+            st.setTimestamp(4, new Timestamp(post.getDate().getTime()));
+            st.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Метод осуществляет поиск всех объявлений в базе.
+     * @return Список объявлений
+     */
+    @Override
+    public List<Post> getAll() {
+        List<Post> posts = new ArrayList<>();
+        try (Statement st = cnn.createStatement();
+             ResultSet rs = st.executeQuery("select * from post")) {
+            while (rs.next()) {
+                Post post = new Post();
+                post.setTitle(rs.getString("name"));
+                post.setDescription(rs.getString("text"));
+                post.setLink(rs.getString("link"));
+                post.setDate(rs.getTimestamp("created"));
+                posts.add(post);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new IllegalStateException("Ошибка поиска объявлений");
+        }
+        return posts;
+    }
+
+    /**
+     * Метод ищет объявление по его ИД.
+     * @param id идентификационный номер
+     * @return Объявление
+     */
+    @Override
+    public Post findById(String id) {
+        Post post = null;
+        try (PreparedStatement st = cnn.prepareStatement("select * from post where id = ?")) {
+            st.setInt(1, Integer.parseInt(id));
+            ResultSet rs = st.executeQuery();
+            if (rs.next()) {
+                post = new Post();
+                post.setTitle(rs.getString("name"));
+                post.setDescription(rs.getString("text"));
+                post.setLink(rs.getString("link"));
+                post.setDate(rs.getTimestamp("created"));
+            }
+            rs.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new IllegalStateException("Ошибка поиска объявления");
+        }
+        return post;
+    }
+
+    /**
+     * Метод закрывает соединение с базой
+     * @throws Exception Исключение при ошибке закрытия соединения с базой
+     */
+    @Override
+    public void close() throws Exception {
+        if (cnn != null) {
+            cnn.close();
+        }
+    }
+
+    /**
+     * Главный метод программы.
+     * В методе демонстрируется работа класса.
+     * @param args Параметры командной строки
+     */
+    public static void main(String[] args) {
+        String config = "rabbit.properties";
+        Properties cfg = new Properties();
+        try (InputStream input = PsqlStore.class.getClassLoader().getResourceAsStream(config)) {
+            cfg.load(input);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new IllegalStateException("Ошибка чтения файла конфигурации.");
+        }
+        PsqlStore store = new PsqlStore(cfg);
+        List<Post> first = new SqlRuParse().list("https://www.sql.ru/forum/job-offers");
+        first.forEach(store::save);
+        List<Post> second = store.getAll();
+        Post post = store.findById("10");
+    }
+}
